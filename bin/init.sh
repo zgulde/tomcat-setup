7b969f203394aef43c926e8ca6eba9c87b95e055
Zach Gulde <zachgulde@gmail.com>
2018-02-20 21:15:18 -0600

    WIP: create cods share command

--- bin/init.sh
+++ bin/init.sh
@@ -2,16 +2,25 @@
 
 usage() {
 	cat <<-.
-	$(basename $0)
+	$(basename $0) -- This command is for creating new server commands, *not*
+	                  for interacting with a server directly
 
-	init: Initialize a new server
+	Commands:
 
-	You must provide a name for the new server, this name will be the name of
-	the command that is created.
+	init:  Initialize a new server. -- You must provide a name for the new
+	                                   server, this name will be the name of
+	                                   the command that is created.
+
+	share: Add a shared server -- You will need to have ssh access to the
+	                              server, and provide a name for the shared
+	                              server command
 
 	Examples:
 	    $(basename $0) init myserver
 	    $(basename $0) init my-awesome-server
+
+	    $(basename $0) share shared-server
+	    $(basename $0) share some-project-server
 	.
 	exit 1
 }
@@ -30,10 +39,7 @@ SCRIPTS=$BASE_DIR/scripts
 
 source $SCRIPTS/util.sh
 
-if [[ -z $1 || $1 != init || -z $2 ]] ; then
-	usage
-fi
-
+[[ -z $2 ]] && usage
 COMMAND_NAME="$2"
 
 if [[ -e /usr/local/bin/$COMMAND_NAME ]] ; then
@@ -42,10 +48,31 @@ if [[ -e /usr/local/bin/$COMMAND_NAME ]] ; then
 	exit 1
 fi
 
-DATA_DIR="$HOME/.cods/$COMMAND_NAME"
-ENV_FILE="$DATA_DIR/env.sh"
-
-mkdir -p $DATA_DIR/db-backups
-
-source $BASE_DIR/scripts/setup.sh
+case $1 in
+	init)
+		DATA_DIR="$HOME/.cods/$COMMAND_NAME"
+		ENV_FILE="$DATA_DIR/env.sh"
+		mkdir -p $DATA_DIR/db-backups
+		source $BASE_DIR/scripts/setup.sh
+		;;
+	share)
+		read -p "Enter the server's ip address: " ip
+		read -p "Enter your username: " user
+		if ! ssh $user@$ip true ; then
+			echo "Unable to login! Command: ssh $user@$ip true"
+			echo 'Make sure you have access to the server and have the correct'
+			echo 'username and ip address.'
+			exit 1
+		fi
+		ln -s $BASE_DIR/server /usr/local/bin/$COMMAND_NAME
+		DATA_DIR="$HOME/.cods/$COMMAND_NAME"
+		ENV_FILE="$DATA_DIR/env.sh"
+		mkdir -p $DATA_DIR/db-backups
+		echo "ip=$ip" >> $ENV_FILE
+		echo "user=$user" >> $ENV_FILE
+		touch $DATA_DIR/credentials.txt
+		echo "All done! '$COMMAND_NAME' ready to go!"
+		;;
+	*) usage;;
+esac
 
